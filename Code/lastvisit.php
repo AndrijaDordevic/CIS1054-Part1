0995f338f7a6ba51dc225bb7457b6b9d97a81b40
AndrijaDordevic <150381922+AndrijaDordevic@users.noreply.github.com>
2024-03-27 13:23:43 +0100

    Update lastvisit.php

--- Code/lastvisit.php
+++ Code/lastvisit.php
@@ -3,7 +3,7 @@
 
   if (!isset($_SESSION['first time'])) {// Checking the session if it is the first time entered by the user
         $_SESSION['first time'] = time();// Setting the time of the session start
-        echo "Welcome to this page😊!";// Showing the user the wecome page welcome page
+        echo "Welcome to this page!";// Showing the user the wecome page welcome page
 
   } else {// To show the ammount of time passed when useing the website
         $time_elapsed = time() - $_SESSION['first time'];// The total time of the usage of the program. ex:15min
