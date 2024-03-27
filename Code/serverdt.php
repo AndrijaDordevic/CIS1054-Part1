92725b3fbbb9db09a20857bd62a6902e7edb5e16
AndrijaDordevic <150381922+AndrijaDordevic@users.noreply.github.com>
2024-03-27 16:22:27 +0100

    Update serverdt.php

--- Code/serverdt.php
+++ Code/serverdt.php
@@ -3,6 +3,20 @@
 $serverDateTime = date('Y-m-d H:i:s');
 
 // Output the server date and time
-echo "The current date and time is: ";
-echo $serverDateTime;
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parameters Stored</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <h1>The current date and time is: </h1>
+        <?php echo $serverDateTime; ?>
+    </div>
+</body>
+</html>
