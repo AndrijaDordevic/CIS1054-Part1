faeb56e1c0b3a374baf2e0efaa9448678c12cf89
AndrijaDordevic <150381922+AndrijaDordevic@users.noreply.github.com>
2024-03-27 16:22:13 +0100

    Update lastvisit.php

--- Code/lastvisit.php
+++ Code/lastvisit.php
@@ -1,12 +1,27 @@
 <?php
-  session_start();
+session_start();
 
-  if (!isset($_SESSION['first time'])) {// Checking the session if it is the first time entered by the user
-        $_SESSION['first time'] = time();// Setting the time of the session start
-        echo "Welcome to this page!";// Showing the user the wecome page welcome page
-
-  } else {// To show the ammount of time passed when useing the website
-        $time_elapsed = time() - $_SESSION['first time'];// The total time of the usage of the program. ex:15min
-        echo "You used this page the first time " . $time_elapsed . " seconds ago";// Showing the user the time they accessed the website the first time
-  }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Last Visit</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <?php
+        if (!isset($_SESSION['first_visit'])) {
+            $_SESSION['first_visit'] = time();
+            echo "<p class='welcome'>Welcome to the page!</p>";
+        } else {
+            $seconds_ago = time() - $_SESSION['first_visit'];
+            echo "<p class='visit'>You first used this page $seconds_ago seconds ago.</p>";
+        }
+        ?>
+    </div>
+</body>
+</html>
