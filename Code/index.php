5a4805cedb6ef6c40561a1f7d05e81002f5f1335
AndrijaDordevic <150381922+AndrijaDordevic@users.noreply.github.com>
2024-03-27 16:22:01 +0100

    Update index.php

--- Code/index.php
+++ Code/index.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
-    <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-            <link rel='stylesheet' href='styles.css'> 
-        </head>
-        <body>
-            <h1>Welcome</h1>
-            <?php include "serverdt.php"?>
-            <p></p>
-            <?php include "lastvisit.php"?>
-
-        </body>
-    </html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Scripts</title>
+    <link rel='stylesheet' href='styles.css'>
+</head>
+<body>
+    <h1>Welcome!</h1>
+    <ul>
+        <li><a href="serverdt.php">Get Current Date and Time</a></li>
+        <li><a href="lastvisit.php">Track Last Visit</a></li>
+        <li><a href="processrequest.html">Capture Request Parameters</a></li>
+        <li><a href="readsession.php">Read Session Variables</a></li>
+    </ul>
+</body>
+</html>
